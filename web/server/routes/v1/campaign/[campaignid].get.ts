41f4561cf8078a47fbf35f0bf0000d1fdf8bc5c1
Ava's DAO <support@avasdao.org>
2023-08-15 01:40:36 -0400

    Request txs.

--- web/server/routes/v1/campaign/[campaignid].get.ts
+++ web/server/routes/v1/campaign/[campaignid].get.ts
@@ -1,6 +1,10 @@
 /* Import modules. */
+import moment from 'moment'
 import PouchDB from 'pouchdb'
-import { getAddressHistory } from '@nexajs/rostrum'
+import {
+    getAddressHistory,
+    getTransaction,
+} from '@nexajs/rostrum'
 
 /* Initialize databases. */
 const campaignsDb = new PouchDB(`http://${process.env.COUCHDB_USER}:${process.env.COUCHDB_PASSWORD}@127.0.0.1:5984/campaigns`)
@@ -8,7 +12,7 @@ const campaignsDb = new PouchDB(`http://${process.env.COUCHDB_USER}:${process.en
 /* Initialize (data) cache. */
 let cache = {}
 
-const CACHE_REFRESH_DELAY = 30000 // default: 30 seconds
+const CACHE_REFRESH_DELAY = 30 // default: 30 seconds
 
 export default defineEventHandler(async (event) => {
     let campaign
@@ -17,10 +21,11 @@ export default defineEventHandler(async (event) => {
     let history
     let receiver
     let response
+    let txs
 
     /* Set campaign id. */
     campaignid = event.context.params?.campaignid
-    console.log('CAMPAIGNID', campaignid)
+    // console.log('CAMPAIGNID', campaignid)
 
     if (!campaignid) {
         return {}
@@ -29,7 +34,7 @@ export default defineEventHandler(async (event) => {
     response = await campaignsDb
         .get(campaignid)
         .catch(err => console.error(err))
-    console.log('CAMPAIGN', response)
+    // console.log('CAMPAIGN', response)
 
     if (!response) {
         return {}
@@ -48,13 +53,36 @@ export default defineEventHandler(async (event) => {
 
     receiver = campaign.receiver
 
-    history = await getAddressHistory(receiver)
-        .catch(err => console.error(err))
-    console.log('HISTORY', history)
+    if (!cache[campaignid]?.updatedAt || cache[campaignid].updatedAt + CACHE_REFRESH_DELAY < moment().unix()) {
+        history = await getAddressHistory(receiver)
+            .catch(err => console.error(err))
+        // console.log('HISTORY', history)
 
-    cache.history = history
+        txs = []
 
-    campaign.history = history
+        for (let i = 0; i < history.length; i++) {
+            const txid = history[i].tx_hash
+
+            const tx = await getTransaction(txid)
+                .catch(err => console.error(err))
+
+            txs.push({
+                hash: tx.hash,
+                height: tx.height,
+                fee: tx.fee,
+            })
+        }
+
+        cache[campaignid] = {
+            txs,
+            updatedAt: moment().unix(),
+        }
+    } else {
+        console.log('***LOADED FROM CACHE***')
+    }
+
+    /* Update cache. */
+    campaign.cache = cache[campaignid]
 
     /* Return campaigns. */
     return campaign

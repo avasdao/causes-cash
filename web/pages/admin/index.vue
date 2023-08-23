2f873c5aa3baa809f561c4933dce0a993c053464
Ava's DAO <support@avasdao.org>
2023-08-22 23:00:21 -0400

    Add auth window.

--- web/pages/admin/index.vue
+++ web/pages/admin/index.vue
@@ -27,12 +27,6 @@ const System = useSystemStore()
 
 <template>
     <main class="max-w-7xl mx-auto py-5">
-        <h1 class="text-5xl font-medium">
-            Admin
-        </h1>
-
-        <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id eius voluptatem minus natus at eveniet dolorum eos mollitia, maxime animi excepturi harum omnis illum odit recusandae pariatur! Unde, explicabo molestias.
-        </p>
+        <AuthWin />
     </main>
 </template>
